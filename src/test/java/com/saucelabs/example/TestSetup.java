0af9500d4e04a9cf88c8da6dd20f3257df278b8b
Alex Griffen <radhack242@gmail.com>
2019-07-02 06:57:10 -0700

    add all the updates

--- src/test/java/com/saucelabs/example/TestSetup.java
+++ src/test/java/com/saucelabs/example/TestSetup.java
@@ -1,8 +1,12 @@
 package com.saucelabs.example;
 
 import com.saucelabs.example.util.ResultReporter;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,16 +17,22 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testobject.appium.junit.TestObjectAppiumSuite;
+import org.testobject.appium.junit.TestObjectAppiumSuiteWatcher;
+import org.testobject.appium.junit.TestObjectTestResultWatcher;
+import org.testobject.rest.api.appium.common.TestObject;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
+@RunWith(TestObjectAppiumSuite.class)
 /**
  * Created by grago on 28/02/2017.
  */
 public class TestSetup {
 
-    private AndroidDriver driver;
+    private AppiumDriver driver;
+
     private ResultReporter reporter;
 
     @BeforeMethod
@@ -31,13 +41,21 @@ public class TestSetup {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("testobject_api_key", System.getenv("TESTOBJECT_API_KEY"));
+        capabilities.setCapability("testobject_api_key", "D7348099E5FC44458E807282AF87FDB4"); //0462524C86944DE39F74A680E1470099
         capabilities.setCapability("testobject_device", device);
-        capabilities.setCapability("testobject_suite_name", "allTheTests");
-        capabilities.setCapability("testobject_test_name", "twoPlusTwoOperation_alex");
+        // capabilities.setCapability("platformVersion", "8");
+        capabilities.setCapability("frameworkVersion","1.13.0");
+        // capabilities.setCapability("testobject_suite_name", "allTheTests");
+//        capabilities.setCapability("testobject_test_report_id", resultWatcher.getTestReportId());
+        // capabilities.setCapability("testobject_test_name", "twoPlusTwoOperation_alex");
+        // capabilities.setCapability("recordDeviceVitals", true);
+        // capabilities.setCapability("automationName", "uiautomator2");
+        capabilities.setCapability("networkSpeed", "gsm");
+        // capabilities.setCapability("autoAcceptAlerts", true);
 
-        driver = new AndroidDriver(new URL(System.getenv("APPIUM_URL")), capabilities);
+        driver = new AndroidDriver(new URL("https://us1.appium.testobject.com/wd/hub"), capabilities);
         reporter = new ResultReporter();
+        // System.out.println("Desired capabilities from TestObject/RDC: " + driver.getCapabilities().asMap());
 
     }
 
@@ -45,14 +63,24 @@ public class TestSetup {
     @Test
     public void twoPlusTwoOperation() throws MalformedURLException {
 
-
         driver.getScreenshotAs(OutputType.BASE64);
+        // driver.findElement(By.id("OK")).click();
+        // driver.switchTo().alert().accept
+        // String cap = driver.getCapabilities().getCapability("testobject_device").toString();
+        // System.out.println("*$&$*$&$*$&$*$&");
+        // System.out.println("Current Device: " + cap);
+        //
+        //
+        // if(driver.getAlertText() != null){
+        //   driver.switchTo().alert().accept(); //this works, though need better if statement
+        //   System.out.println("made it here");
+        // };
+
         /* Get the elements. */
         MobileElement buttonTwo = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/digit2")));
         MobileElement buttonPlus = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/plus")));
         MobileElement buttonEquals = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/equal")));
         MobileElement resultField = (MobileElement)(driver.findElement(By.xpath("//android.widget.EditText[1]")));
-
         driver.getScreenshotAs(OutputType.BASE64);
 
         /* Add two and two. */
@@ -73,6 +101,7 @@ public class TestSetup {
     public void tearDown(ITestResult result) {
         boolean success = result.isSuccess();
         String sessionId = driver.getSessionId().toString();
+        // System.out.println("sessionId is: " + sessionId);
 
         reporter.saveTestStatus(sessionId, success);
         driver.quit();
